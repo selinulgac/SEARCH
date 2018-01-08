15f8818cd603818e0c53bcf8e5d655dba2203b5b
SELİN <selinulgac@hotmail.com>
2018-01-08 21:02:11 +0300

    Add files via upload

---
+++ controllers/backend/SearchController.php
@@ -0,0 +1,112 @@
+<?php
+namespace kouosl\search\controllers\backend;
+use Yii;
+use kouosl\search\models\search;
+use kouosl\search\models\EmergencySearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+/**
+ * searchController implements the CRUD actions for Search model.
+ */
+class SearchController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+    /**
+     * Lists all Search models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $libraryModel = new Search();
+        $dataProvider = $libraryModel->search(Yii::$app->request->queryParams);
+        return $this->render('index', [
+            'libraryModel' => $libraryModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+    /**
+     * Displays a single library model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+    /**
+     * Creates a new Search model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Search();
+		
+		İf ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+    /**
+     * Updates an existing search model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id']);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+    /**
+     * Deletes an existing Search model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+        return $this->redirect(['index']);
+    }
+    /**
+     * Finds the search model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return search the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Search::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}

6a6ac041df17bce69023997a8c96430b76bd077c
SELİN <selinulgac@hotmail.com>
2018-01-08 21:11:16 +0300

    Add files via upload

---
+++ models/AuthorSearch.php
@@ -0,0 +1,41 @@
+<?php
+namespace kouosl\search\models;
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use kouosl\search\models\search;
+/**
+ * EmergencySearch represents the model behind the search form about `kouosl\search\models\Search`.
+ */
+class LibrarySearch extends Search
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+        ];
+    }
+    
+    public function searchAuthor($params)
+    {
+        $query = Search::find();
+        // add conditions that should always apply here
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+        $this->load($params);
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+        ])
+        return $dataProvider;
+    }
+}
---
+++ models/BookSearch.php
@@ -0,0 +1,50 @@
+<?php
+namespace kouosl\search\models;
+use Yii;
+
+class Search extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'book_id';
+    }
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'varchar'],
+            [['user_id'], 'integer','max' => 11],
+            [['isim'], 'string'],
+            [['yayınevi'], 'string'],
+            [['tarih'], 'date'],
+            [['cevirmen'], 'string'],
+            [['yazar_ad'], 'string'],
+            [['yazar_soyad'], 'string'],
+            [['yazar_id'], 'integer'],
+            
+        ];
+    }
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'USER_ID',
+            'isim' => 'ISIM',
+            'yayınevi' => 'YAYINEVI',
+            'tarih' => 'TARIH',
+            'cevirmen' => 'CEVIRMEN',
+            'yazar_ad' => 'YAZAR_AD',
+            'yazar_soyad' => 'YAZAR_SOYAD',
+            'yazar_id' => 'YAZAR_ID',
+			
+        ];
+    }
+}
